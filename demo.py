cc8b3f2ad6a5d63e8ddadb682dd6fac776b04ec4
Lee <294622946@qq.com>
2025-12-01 17:14:07 +0800

    test

--- demo.py
+++ demo.py
@@ -4,5 +4,6 @@ from prefect import flow
 
 @flow(name="test_request", log_prints=True)
 def test_request():
+    raise Exception("test")
     response = requests.get("https://httpbin.org/get")
     print(response.json())
